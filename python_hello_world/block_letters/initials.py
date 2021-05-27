377c371dff7233fbb35f8debb528cfe8c255168f
mikokallas <miko.kallas@gmail.com>
2021-05-27 12:41:03 +0300

    add initials and lovely loveseat exercises

---
+++ python_hello_world/block_letters/initials.py
@@ -0,0 +1,25 @@
+# My name is Miko Kallas.
+"""My ultimate goal in IT is to work with AI in self-moving objects, bionic limbs and/or work with AR in
+ medical procedures to help surgeons and students prepare for the surgeries and exams."""
+
+first_initial = """
+M   M
+MM MM
+MM MM
+M M M
+M   M
+M   M
+M   M
+"""
+
+second_initial = """
+K   K
+K  K 
+K K  
+KK   
+K K  
+K  K 
+K   K
+"""
+
+print(first_initial + second_initial)

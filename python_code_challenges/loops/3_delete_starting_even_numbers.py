6e37a316ba64ec3fb512f81ab7da6b82de6725d0
mikokallas <miko.kallas@gmail.com>
2021-06-27 19:24:50 +0300

    remove redundant parenthesis in ex 3 "Delete Starting Even Numbers" line 19

--- python_code_challenges/loops/3_delete_starting_even_numbers.py
+++ python_code_challenges/loops/3_delete_starting_even_numbers.py
@@ -16,7 +16,7 @@ we will need the following steps:
 # Write your function here
 
 def delete_starting_evens(lst):
-    while (len(lst) > 0 and lst[0] % 2 == 0):
+    while len(lst) > 0 and lst[0] % 2 == 0:
         lst = lst[1:]
     return lst
 

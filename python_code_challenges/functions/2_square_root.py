1924c395e944c4893ace021c1a51137d7c877466
mikokallas <miko.kallas@gmail.com>
2021-06-28 20:26:51 +0300

    add 2nd Python Code Challenges: Functions exercise "Square Root"

---
+++ python_code_challenges/functions/2_square_root.py
@@ -0,0 +1,23 @@
+"""
+2. Square Root.
+
+Another useful function for solving math problems is the square root function. We can create this using similar steps
+from the last problem. The code will look very similar. We need to:
+
+ - Set up the function header for square_root which accepts one parameter
+ - Take the square root of the input value
+ - Return the result
+"""
+
+
+# Write your square_root function here:
+
+def square_root(num):
+    return num ** 0.5
+
+
+# Uncomment these function calls to test your square_root function:
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
